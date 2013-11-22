cac3309cae5a87b398561013076617d91643a6d6
fclamp <fc-lamp@163.com>
2013-11-22 17:50:41 +0800

    all

--- config.inc.php
+++ config.inc.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'DB_HOST' => 'xxxx',
-  'DB_PWD' => 'xxxx',
-  'DB_NAME' => 'xxxx',
-  'DB_USER' => 'xxxx',
-  'DB_PORT' => 'xxxx',
+  'DB_HOST' => '192.168.2.249',
+  'DB_PWD' => '16160',
+  'DB_NAME' => 'www.scxxwh.com',
+  'DB_USER' => 'root',
+  'DB_PORT' => '3306',
   'DB_PREFIX' => 'cms_',
   'DEFAULT_THEME' => 'default',
   'TMPL_ACTION_SUCCESS' => 'public:success',
